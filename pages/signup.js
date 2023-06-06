f54ae5261e9bd57bbc1adc8f422a3bf0dcaa8214
dnwn-9001 <j10511@hanmail.net>
2023-06-06 14:54:46 +0900

    회원가입 페이지

--- pages/signup.js
+++ pages/signup.js
@@ -1,20 +1,46 @@
 import React, { useState, useCallback } from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
-import { Button, Form, Input, Space } from "antd";
+import { Button, Form, Input, Space, Checkbox } from "antd";
 import useInput from "../hooks/useInput";
+import styled from "styled-components";
+
+const ErrorMessage = styled.div`
+  color: red;
+`;
+
+const SubmitBtn = styled(Button)`
+  margin-top: 20px;
+`;
 
 const Signup = () => {
-  const onSubmit = useCallback(() => {});
-
   const [id, onChangeId] = useInput("");
   const [nickname, onChangeNickname] = useInput("");
   const [password, onChangePassword] = useInput("");
 
   const [passwordCheck, setPasswordCheck] = useState("");
-  const onChangePasswordCheck = useCallback((e) => {
-    setPasswordCheck(e.target.value);
+  const [passwordError, setPasswordError] = useState(false);
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
+
+  const [term, setTerm] = useState("");
+  const [termError, setTermError] = useState(false);
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(false);
   }, []);
+
+  const onSubmit = useCallback(() => {
+    if (password !== passwordCheck) return setPasswordError(true);
+    if (!term) return setTermError(true);
+
+    console.log(id, nickname, password);
+  }, [password, passwordCheck, term]);
 
   return (
     <>
@@ -40,6 +66,7 @@ const Signup = () => {
           >
             <Input value={id} onChange={onChangeId} />
           </Form.Item>
+
           <Form.Item
             name="nickname"
             label="닉네임"
@@ -51,6 +78,7 @@ const Signup = () => {
           >
             <Input value={nickname} onChange={onChangeNickname} />
           </Form.Item>
+
           <Form.Item
             name="password"
             label="비밀번호"
@@ -60,8 +88,9 @@ const Signup = () => {
               },
             ]}
           >
-            <Input value={password} onChange={onChangePassword} />
+            <Input.Password value={password} onChange={onChangePassword} />
           </Form.Item>
+
           <Form.Item
             name="passwordCheck"
             label="비밀번호 확인"
@@ -71,11 +100,24 @@ const Signup = () => {
               },
             ]}
           >
-            <Input value={passwordCheck} onChange={onChangePasswordCheck} />
+            <div>
+              <Input.Password
+                value={passwordCheck}
+                onChange={onChangePasswordCheck}
+              />
+              {passwordError && (
+                <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>
+              )}
+            </div>
           </Form.Item>
+
+          <Checkbox checked={term} onChange={onChangeTerm}>
+            약관에 동의합니다.
+          </Checkbox>
+          {termError && <ErrorMessage>약관에 동의하셔야 합니다.</ErrorMessage>}
           <Form.Item>
             <Space>
-              <Button htmlType="submit">제출하기</Button>
+              <SubmitBtn htmlType="submit">가입하기</SubmitBtn>
             </Space>
           </Form.Item>
         </Form>
